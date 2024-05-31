5b8207eed4f2496deb07539a7208ac7bbb4c71fa
fred <zhangqi@tr-ag.com>
2024-05-31 09:18:57 +0800

    hotfix-镜像升级自动化/1.6.2

--- components/the_latest_image_updater.sh
+++ components/the_latest_image_updater.sh
@@ -15,7 +15,6 @@ the_latest_image_updater() {
   # 再次更新软件包列表以包含 backports 包
   echo "Updating package list with backports..."
   sudo apt update
-  sudo apt upgrade -y
 
   # 查找并安装最新的内核镜像和头文件包
   echo "Installing the latest kernel image and headers from backports..."
